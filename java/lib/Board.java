fb71f1ef2a3b26e5ec094e48f8cd5971ed16243c
kwoodard22 <kwoodard22@gmail.com>
2017-05-26 08:53:02 -0700

    Work in progress; Board & Cell
    - I need a more efficient method than two 2D arrays
    - Exploring 2D & 1D but haven’t solved neighbor indexes of 1D. Might have to Google…

--- java/lib/Board.java
+++ java/lib/Board.java
@@ -11,36 +11,47 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdDraw;
 
-// Create board & randomly generate live cells
-// Draw board
-// Update board 
-// Go through cells & check neighbors & update accordingly
-
 public class Board {
-    int n;
+    int numAlive;
     boolean[][] grid;
+    boolean[][] gridCopy;
     
     // Create n-by-n grid with random live cells placed
     public Board(int gridSize, int liveCells) {
         int n = gridSize;
-        int c = liveCells;
-        grid = new boolean[n][n];
+        numAlive = liveCells;
         
+        grid = new boolean[n][n];
+        cells = new boolean[n*n];
+        
+        // Randomly generate live cells
         for (int i = 0; i < c;) {
             int row = StdRandom.uniform(c);
             int col = StdRandom.uniform(c);
             if (!grid[row][col]) {
                 grid[row][col] = true;
+                gridCopy[row][col] = true;
+                numAlive++;
                 i++;
             }
         }
     }
     
-    public void updatePopulation(int[] cellIndices) {
-        int[] cells = cellIndices;
+    public void updatePopulation() {
+        Cell cell = new Cell;
         
-        for (i = 0; i < cells; i++) {
-            
+        for (r = 0; r < n; r++) {
+            for (c = 0; c < n; c++) {
+                boolean alive = cell.isAlive(grid[r][c], numberOfNeighbors(r, c))
+                gridCopy[r][c] = alive;
+                // if dead & now alive
+                if (!grid[r][c] && alive) { 
+            }
+        }
+        for (r = 0; r < n; r++) {
+            for (c = 0; c < n; c++) {
+                grid[r][c] = gridCopy[r][c]
+            }
         }
     }
         
@@ -53,5 +64,27 @@ public class Board {
         Board b = new Board(n, c);
         b.printGrid();
         
+    }
+    
+    // PRIVATE
+    private numberOfNeighbors(int row, int col) {
+        int neighbors = 0;
+        //topLeft
+        if ((col > 0 && row > 0) && grid[row - 1][col - 1]) { neighbors++; }
+        // top
+        if ((col > 0) && grid[row][col - 1]) { neighbors++; }
+        // topRight
+        if ((col < (N-1) && row > 0) && grid[row - 1][col + 1] { neighbors++; }
+        // right
+        if (col < (N-1) && grid[row][col + 1]) { neighbors++; }
+        // bottomRight
+        if ((row < (N-1) && col < (N-1)) && grid[row + 1][col]) { neighbors++; }
+        // bottom
+        if (row < (N-1) && grid[row + 1][col] { neighbors++; }
+        // bottomLeft
+        if ((row < (N-1) && col > 0) && grid[row + 1][col - 1]) { neighbors++; }
+        // left
+        if (col > 0 && grid[row][col - 1]) { neighbors++; }
+        return neighbors;
     }
 }
--- java/lib/Cell.java
+++ java/lib/Cell.java
@@ -1,2 +1,49 @@
-public class Cell {
+public class Cell {  
+    
+    public void Cell () {
+    }
+    
+    public isAlive(int numberOfNeighbors) {
+        Int neighbors = numberOfNeighbors;
+        
+        // if currently alive
+        if (population[cellIndex]) {
+            if (neighbors < 2) { return false};
+            if (neighbors >= 2 && neighbors <= 3) { return true; }
+            if (neighbors > 3) { return false; }
+        // if currently dead
+        } else {
+            if (neighbors == 3) { return true; }
+        }
+    }
+    
 }
+
+/*
+ * 
+  if 
+  1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+      if (neighbors < 2) { return false};
+      
+  1. Any live cell with two or three live neighbours lives on to the next generation.
+      if (neighbors > 1 && neighbors < 4) { return true; }
+      
+  1. Any live cell with more than three live neighbours dies, as if by overpopulation.
+      if (neighbors > 3) { return false; }
+      
+  1. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+*/
+
+/* 
+ * [ 
+ *   [false, false, false, false],
+ *   [true,  true,  false, false],
+ *   [true,  false, false, true],
+ *   [false, false, false, true]
+ * ]
+ * 
+ * [ false, false, false, false, true, true, false, false, true, false, false, true, false, false, false, true]
+ * 
+ * 
+ * 
+ */
